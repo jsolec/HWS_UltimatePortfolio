8edcfaf40b18633da9a5842805ffccb60997bd4a
JesusSoleC <jesus@futureworkshops.com>
2022-02-16 16:23:31 +0100

    First commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Jesús Solé on 16/2/22.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
