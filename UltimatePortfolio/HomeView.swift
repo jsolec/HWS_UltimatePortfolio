37aa1cd54d5ed38b06270f430c6935869274fca1
JesusSoleC <jesus@futureworkshops.com>
2022-02-17 17:32:48 +0100

    Setup basic navigation

---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Jesús Solé on 17/2/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    self.dataController.deleteAll()
+                    try? self.dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ UltimatePortfolio/ProjectsView.swift
@@ -0,0 +1,49 @@
+//
+//  ProjectsView.swift
+//  UltimatePortfolio
+//
+//  Created by Jesús Solé on 17/2/22.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    let showClosedProjects: Bool
+    let projects: FetchRequest<Project>
+    
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        self.projects = FetchRequest<Project>(
+            entity: Project.entity(),
+            sortDescriptors: [
+                NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)
+            ],
+            predicate: NSPredicate(format: "closed = %d", showClosedProjects)
+        )
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(self.projects.wrappedValue) { project in
+                    Section(header: Text(project.title ?? "")) {
+                        ForEach(project.items?.allObjects as? [Item] ?? []) { item in
+                            Text(item.title ?? "")
+                        }
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle(self.showClosedProjects ? "Closed Projects" : "Open Projects")
+        }
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    static var previews: some View {
+        ProjectsView(showClosedProjects: false)
+            .environment(\.managedObjectContext, self.dataController.container.viewContext)
+            .environmentObject(self.dataController)
+    }
+}
